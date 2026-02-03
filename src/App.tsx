3e98191fdb248f253a80d0d901a855b5cefc132c
Yash Chitlangi <yashchitlangi@192.168.1.31>
2026-02-03 21:25:50 +0530

    fix react router basename for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/bhoomisoni">
+
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
